74ed7988fef16693361847035085ed73f3e8035c
peggy <peggy.zhang@gov.bc.ca>
2019-11-03 15:40:41 -0800

    some settings

--- pw-reset-api/Repositories/OracleUserRepository.cs
+++ pw-reset-api/Repositories/OracleUserRepository.cs
@@ -116,6 +116,41 @@ namespace pw_reset_api.Repositories
             }
         }
 
+        private void ConfigOracle()
+        {
+                        // This sample demonstrates how to use ODP.NET Core Configuration API
+
+            // Add connect descriptors and net service names entries.
+            OracleConfiguration.OracleDataSources.Add("orclcdb", "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORCLCDB.localdomain)(SERVER=dedicated)))");
+            //OracleConfiguration.OracleDataSources.Add("orcl", "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=<hostname or IP>)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=<service name>)(SERVER=dedicated)))");
+
+            // Set default statement cache size to be used by all connections.
+            OracleConfiguration.StatementCacheSize = 25;
+
+            // Disable self tuning by default.
+            OracleConfiguration.SelfTuning = false;
+
+            // Bind all parameters by name.
+            OracleConfiguration.BindByName = true;
+
+            // Set default timeout to 60 seconds.
+            OracleConfiguration.CommandTimeout = 60;
+
+            // Set default fetch size as 1 MB.
+            OracleConfiguration.FetchSize = 1024 * 1024;
+
+            // Set tracing options
+           // OracleConfiguration.TraceOption = 1;
+            //OracleConfiguration.TraceFileLocation = @"D:\traces";
+            // Uncomment below to generate trace files
+            //OracleConfiguration.TraceLevel = 7;
+            
+            // Set network properties
+            OracleConfiguration.SendBufferSize = 8192;
+            OracleConfiguration.ReceiveBufferSize = 8192;
+            OracleConfiguration.DisableOOB = true;
+        }
+
         public bool IsEmailValid(UserEmailInfo userInfo)
         {
             try
@@ -165,6 +200,7 @@ namespace pw_reset_api.Repositories
                 return false;
             }
         }
+
 
         public bool IsValidOracleUser(string username) 
         {
@@ -434,6 +470,7 @@ namespace pw_reset_api.Repositories
             _logger.LogDebug("create connection" + connectionString);
             var conn = new Oracle.ManagedDataAccess.Client.OracleConnection(connectionString);
             _logger.LogInformation("oracle connection created.");
+            //ConfigOracle();
             return conn;
         }
     }
