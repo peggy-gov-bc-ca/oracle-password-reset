c7208925231cdea02601f3401a8e2c9402158c02
peggy <peggy.zhang@gov.bc.ca>
2019-11-06 14:46:54 -0800

    some settings

--- common/Constants.cs
+++ common/Constants.cs
@@ -7,13 +7,13 @@ namespace common
     public class Constants
     {
         //for container
-        //public static string API_BASE_URL = "http://pw-reset-api/api/";
-        //public static string ORACLE_DATA_SOURCE = "database:1521/ORCLCDB.localdomain";
+        public static string API_BASE_URL = "http://pw-reset-api/api/";
+        public static string ORACLE_DATA_SOURCE = "database:1521/ORCLCDB.localdomain";
 
         //for local debugging vs
-        public static string API_BASE_URL = "http://localhost:5000/api/";
+        //public static string API_BASE_URL = "http://localhost:5000/api/";
         //public static string ORACLE_DATA_SOURCE = "localhost:1521/db.AMERICAS.GLOBAL.NTTDATA.COM";
-        public static string ORACLE_DATA_SOURCE = "localhost:1521/ORCLCDB.localdomain";
+        //public static string ORACLE_DATA_SOURCE = "localhost:1521/ORCLCDB.localdomain";
 
         public static string HOST_NAME = "localhost";
         public static int VERIFY_CODE_VALID_MINS = 5;
